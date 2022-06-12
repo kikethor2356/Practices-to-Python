4ac1fc9b67cc3cc0718dc95dd7b7e3f1c3ec76e6
kikethor2356 <107217996+kikethor2356@users.noreply.github.com>
2022-06-12 13:42:20 -0500

    Add files via upload

---
+++ calculos_generales.py
@@ -0,0 +1,17 @@
+def sumar(op1, op2):
+	print("El resulatado de la suma es:", op1 + op2)
+
+def restar(op1,op2):
+	print("El resulatado de la resta es :", op1-op2)
+
+def multiplicar(op1,op2):
+	print("El resulatado de la multiplicacion es:", op1*op2)
+
+def dividir(op1,op2):
+	print("El resulatado de la division es:",op1/op2)	
+
+def potencia(base,exponente):
+	print("El resulatado de la suma es:", base**exponente)	
+
+def redondear(numero):
+	print("El resulatado de la suma es:", round(numero))
---
+++ redondea_y_potencias.py
@@ -0,0 +1,5 @@
+def potencia(base,exponente):
+	print("El resulatado de la suma es:", base**exponente)	
+
+def redondear(numero):
+	print("El resulatado de la suma es:", round(numero))
