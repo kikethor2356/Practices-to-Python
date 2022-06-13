bee9727a71594c0b156b42cee23c5d60294422a3
kikethor2356 <107217996+kikethor2356@users.noreply.github.com>
2022-06-13 17:38:12 -0500

    Add files via upload

---
+++ Trabajo_serializacion.py
@@ -0,0 +1,22 @@
+import pickle
+
+fichero=open("lista_nombres}", "rb")
+
+lista=pickle.load(fichero)
+
+print(lista)
+
+
+
+
+"""
+lista_nombres=["Pedro", "Ana", "Maria", "Isabel"]
+
+fichero_binario=open("lista_nombres}", "wb")
+
+pickle.dump(lista_nombres, fichero_binario)
+
+fichero_binario.close()
+
+del (fichero_binario) 
+"""
